08f60fce4dc2488370b4bc96bb1cada57d19271e
Parallax <eleanor0401@gmail.com>
2026-08-25 15:00:22 -0600

    Commit inicial: estructura base del proyecto SistemaGestionEstudiantes

---
+++ src/main/java/sistemagestionestudiantes/main/sistemagestionestudiantes/SistemaGestionEstudiantes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sistemagestionestudiantes.main.sistemagestionestudiantes;
+
+/**
+ *
+ * @author Parallax
+ */
+public class SistemaGestionEstudiantes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
